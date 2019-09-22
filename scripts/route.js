0b20df549f65bb86e266abec8cbf438d3c4d70ec
Edik Baghdasaryan <edik.baghdasaryan@volo.global>
2019-09-23 03:45:14 +0400

    Did major changes regarding layout

--- scripts/route.js
+++ scripts/route.js
@@ -1,7 +1,18 @@
+var currentContent;
+
 $('nav li').click(function () {
+    var content = $(this).data('content');
+    if (content && currentContent === content)
+        return;
+    
+    currentContent = content;
+
+    $("nav li").removeClass("active");
+    $("nav a").removeClass("active");
+    $(this).addClass("active");
     $.ajax({
         type: 'GET',
-        url: 'includes/' + $(this).data('content') + '.html',
+        url: 'includes/' + content + '.html',
         dataType: 'html',
         success: function (response) {
             $('.canc').html(response);
